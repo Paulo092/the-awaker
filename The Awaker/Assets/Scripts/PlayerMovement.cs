757dfd18779f1e8f14bcc7a196fe3e4ad6e337a0
Neuromancer <psolsilva@gmail.com>
2023-02-17 12:32:05 -0300

    :sparkles: feat: Animação e Scripts Provisórios
    Adicionando animações para personagem ocioso e caminhando. Adição de alguns scripts de movimento provisórios.

--- The Awaker/Assets/Scripts/PlayerMovement.cs
+++ The Awaker/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 0.005f;
+    public Animator animator; 
+
+    private bool facingRight;
 
     void Start() {
         speed = 0.005f;
     }
 
+    private void Flip(float direction)
+	{
+        if((direction > 0 && !facingRight) || (direction < 0 && facingRight) || direction == 0f) return;
+        else {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1f;
+            transform.localScale = theScale;
+
+            facingRight = !facingRight;
+        }
+	}
+
     void Update() {
+
+        if(Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) animator.SetFloat("Speed", 1);
+        else animator.SetFloat("Speed", 0);
+
+        Flip(Input.GetAxis("Horizontal"));
+
         this.transform.position = new Vector3(
             this.transform.position.x + Input.GetAxis("Horizontal") * speed, 
             this.transform.position.y + Input.GetAxis("Vertical") * speed, 
